92e81c0626f7f1c534d63bae40313df09db33695
Mohit Garg <mohitg@iitrpr.ac.in>
2015-01-23 18:34:47 +0530

    Code Cleaned

--- FunSoft/src/com/example/funsoft/MainActivity.java
+++ FunSoft/src/com/example/funsoft/MainActivity.java
@@ -74,13 +74,11 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		private LayoutInflater mInflater;
 
 		public EfficientAdapter(Context context) {
-			System.out.println("rsexchj,bghfgx");
 			mInflater = LayoutInflater.from(context);
 		}
 
 		public View getView(int position, View convertView, ViewGroup parent) {
 
-			System.out.println("hf23456789hgfv");
 			ViewHolder holder;
 			if (convertView == null) {
 				convertView = mInflater.inflate(R.layout.list_item, null);
@@ -97,14 +95,9 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 				holder = (ViewHolder) convertView.getTag();
 			}
 
-			System.out.println("hfhgfv");
 			holder.title.setText(resultList.get(position).title);
 			holder.description.setText(resultList.get(position).desription);
-			// holder.category.setText(resultList.get(position).category);
 			holder.size.setText(resultList.get(position).size);
-			// holder.title.setText(resultList.title);
-
-			// holder.text2.setText(country[position]);
 
 			return convertView;
 		}
@@ -139,28 +132,20 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		public void notifyDataSetChanged() {
 			System.out.println("Called");
 			super.notifyDataSetChanged();
-			// _NotifyOnChange = true;
 		}
 
 	}
 
 	Button download;
-	// TextView status;
 	EditText search;
 	ListView results;
 	static ArrayList<myData> resultList = new ArrayList<myData>();
-	// ArrayAdapter<String> resultAdapter;
 
 	public static final int DIALOG_DOWNLOAD_PROGRESS = 0;
 
 	String query = "http://10.20.87.91/check.php";
 	List<NameValuePair> nameValuePairs;
 	HttpResponse response;
-
-	// private ProgressBar mProgress;
-	// private int mProgressStatus = 0;
-
-	// private Handler mHandler = new Handler();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -169,8 +154,6 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		download = (Button) findViewById(R.id.download_btn);
 		search = (EditText) findViewById(R.id.searchBox);
 		results = (ListView) findViewById(R.id.listView_results);
-		// resultAdapter = new ArrayAdapter<String>(this,
-		// android.R.layout.simple_list_item_1, resultList);
 		results.setAdapter(new EfficientAdapter(this));
 		results.setOnItemClickListener(new OnItemClickListener() {
 
@@ -178,18 +161,11 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 			public void onItemClick(AdapterView<?> adapter, View arg1,
 					int position, long arg3) {
 				// TODO Auto-generated method stub
-				// System.out.println(position);
-				// System.out.println(resultList.get(position).link);
-				// String data = (String) adapter.getItemAtPosition(position);
-				// data = data.split("Link")[1];
 				String data = resultList.get(position).link;
-				String titledata=resultList.get(position).title;
-				System.out.println(data);
-				startDownload(data,titledata);
+				String titledata = resultList.get(position).title;
+				startDownload(data, titledata);
 			}
 		});
-		// status = (TextView) findViewById(R.id.status);
-		// mProgress = (ProgressBar) findViewById(R.id.downloadBar);
 		download.setOnClickListener(this);
 	}
 
@@ -219,9 +195,6 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		switch (id) {
 		case R.id.download_btn:
 			searchData(v, search.getText().toString());
-			// status.setText("Button Clicked!");
-			// mProgress.setProgress(50);
-			// startDownload();
 			break;
 		}
 	}
@@ -229,7 +202,6 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 	private void searchData(View v, final String search) {
 
 		// TODO Auto-generated method stub
-		System.out.println("asdsa78d");
 		new ProgressDialog(v.getContext());
 		final ProgressDialog p = ProgressDialog.show(v.getContext(),
 				"Waiting for Server", "Accessing Server");
@@ -240,7 +212,6 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 			public void run() {
 				try {
 
-					System.out.println("asdsad");
 					HttpClient httpclient = new DefaultHttpClient();
 					HttpPost httppost = new HttpPost(query);
 
@@ -249,20 +220,14 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 							"search"));
 					nameValuePairs.add(new BasicNameValuePair("query", search));
 					httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-					System.out.println("asdsad");
-					System.out.println(nameValuePairs);
 					response = httpclient.execute(httppost);
-					System.out.println(response);
 					ResponseHandler<String> responseHandler = new BasicResponseHandler();
 					final String response = httpclient.execute(httppost,
 							responseHandler);
 
-					System.out.println(response);
 					runOnUiThread(new Runnable() {
 						public void run() {
 							p.dismiss();
-							// Toast.makeText(getApplicationContext(),"Success",
-							// Toast.LENGTH_SHORT).show();
 							displayResults(response);
 						}
 					});
@@ -278,13 +243,12 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		thread.start();
 	}
 
-	private void startDownload(String url,String title) {
+	private void startDownload(String url, String title) {
 		// TODO Auto-generated method stub
-		// String url = "http://cse.iitrpr.ac.in/ckn/courses/s2015/q1.pdf"; //
 		// your
 		// download
 		// url
-		new DownloadFileAsync().execute(url,title);
+		new DownloadFileAsync().execute(url, title);
 	}
 
 	class DownloadFileAsync extends AsyncTask<String, String, String> {
@@ -292,30 +256,25 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		@Override
 		protected void onPreExecute() {
 			super.onPreExecute();
-			// showDialog(DIALOG_DOWNLOAD_PROGRESS);
 		}
 
 		@Override
 		protected String doInBackground(String... aurl) {
 			int count;
-			System.out.println(aurl);
-			System.out.println("LOL");
 			Intent intent = new Intent();
-			PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, Intent.FLAG_ACTIVITY_NEW_TASK);
+			PendingIntent pendingIntent = PendingIntent.getActivity(
+					getApplicationContext(), 0, intent,
+					Intent.FLAG_ACTIVITY_NEW_TASK);
 
 			final NotificationManager mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		//	 NotificationCompat.Builder builder = new
-		//	 NotificationCompat.Builder(getApplicationContext());
 			final NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
-				getApplicationContext());
-			mBuilder.setContentTitle("Downloading "+aurl[1])
+					getApplicationContext());
+			mBuilder.setContentTitle("Downloading " + aurl[1])
 					.setContentText("Download in progress")
 					.setSmallIcon(R.drawable.video)
-					.setTicker("Downloading "+aurl[1])
-					.setProgress(100, 10, false)
-				//	.setWhen(System.currentTimeMillis())
+					.setTicker("Downloading " + aurl[1])
+					.setProgress(100, 0, false)
 					.setContentIntent(pendingIntent);
-	//		.setProgressBar(100, 100, false);
 			try {
 
 				URL url = new URL(aurl[0]);
@@ -323,38 +282,31 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 				conexion.connect();
 
 				int lenghtOfFile = conexion.getContentLength();
-				Log.d("ANDRO_ASYNC", "Lenght of file: " + lenghtOfFile);
-				System.out.println("LOL");
+
 				File SDCardRoot = Environment.getExternalStorageDirectory();
-				System.out.println("LOL");
+
 				// create a new file, to save the downloaded file
 				File file = new File(SDCardRoot, "downloaded_file123.pdf");
-				System.out.println("LOL"+file);
-				mNotifyManager.notify(0, mBuilder.build());
+
 				InputStream input = new BufferedInputStream(url.openStream());
-				System.out.println("LOLb"+file.canWrite()+file.isFile());
-				mBuilder.setProgress(100, 17, false);
+
 				mNotifyManager.notify(0, mBuilder.build());
-				System.out.println("LOLb"+file.canWrite()+file.isFile());
-				mNotifyManager.notify(0, mBuilder.build());
+
 				OutputStream output = new FileOutputStream(file); // save file
 																	// in SD
 																	// Card
-				System.out.println("LOLa");
-		//		mBuilder.setProgress(100, 17, false);
-				System.out.println("LOL");
+
 				int mProgressStatus;
 				byte data[] = new byte[1024];
 
 				long total = 0;
-				
-				mNotifyManager.notify(0, mBuilder.build());
-//				mNotifyManager.notify();
+
 				while ((count = input.read(data)) != -1) {
 					total += count;
 					mProgressStatus = (int) ((total * 100) / lenghtOfFile);
 					mBuilder.setProgress(100, mProgressStatus, false);
-					// mProgress.setProgress(mProgressStatus);
+					mNotifyManager.notify(0, mBuilder.build());
+
 					System.out
 							.println("Downloading..." + mProgressStatus + "%");
 					output.write(data, 0, count);
@@ -378,23 +330,16 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 	void displayResults(String response) {
 		resultList.clear();
 		try {
-			System.out.println("lol");
+
 			JSONArray j = new JSONArray(response);
 			for (int i = 0; i < j.length(); ++i) {
 
-				// title.add(j.getJSONObject(i).getString("title"));
-				// String t=
 				resultList.add(new myData(
 						j.getJSONObject(i).getString("title"), j.getJSONObject(
 								i).getString("description"), j.getJSONObject(i)
 								.getString("size"), j.getJSONObject(i)
 								.getString("category"), j.getJSONObject(i)
 								.getString("link")));
-				// resultList.add("Title: "
-				// + j.getJSONObject(i).getString("title")
-				// + "\nDescription: "
-				// + j.getJSONObject(i).getString("description")
-				// + "\nLink" + j.getJSONObject(i).getString("link"));
 			}
 			// results.;
 			results.setAdapter(new EfficientAdapter(this));
